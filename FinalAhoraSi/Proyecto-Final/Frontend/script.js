3db411aaf52360bf9fdf7dd4019599a794b34774
salomegarces <salitogarces18@hotmail.com>
2024-06-07 20:25:28 -0500

    appcompleta

--- FinalAhoraSi/Proyecto-Final/Frontend/script.js
+++ FinalAhoraSi/Proyecto-Final/Frontend/script.js
@@ -52,3 +52,46 @@ document.addEventListener("DOMContentLoaded", () => {
         registerUser(peticion);
     });
 });
+
+
+//Funcion para iniciar sesion
+document.getElementById("btn-iniciar").addEventListener("click", function(event) {
+    event.preventDefault(); // Evitar envío del formulario
+
+    // Obtener el nombre de usuario y la contraseña ingresados por el usuario
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Llamar a la función de autenticación con los datos ingresados por el usuario
+    authenticateUser(username, password);
+});
+
+
+btnSignIn.addEventListener("click",()=>{
+container.classList.remove("toggle");
+});
+btnSignUp.addEventListener("click",()=>{
+container.classList.add("toggle");
+});
+
+// Función para autenticar al usuario
+function authenticateUser(username, password) {
+    // Datos de usuario estáticos (para ejemplo)
+    const users = [
+        { username: "salomegarces", password: "cont1" },
+        { username: "usuario2", password: "contraseña2" },
+        {username:'cristian@gmail.com', password:'123456'}
+    ];
+
+    // Buscar al usuario en la lista de usuarios
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        // Si el usuario es encontrado, la autenticación es exitosa
+        // Redirigir a la página de reservas
+        window.location.href = "buscar.html";
+    } else {
+        // Si el usuario no es encontrado, mostrar un mensaje de error
+        alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+    }
+}
